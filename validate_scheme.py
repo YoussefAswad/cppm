9e2d06c8f5e0f95eb6ffe48b80fd09bb6102da89
YoussefAswad <85849220+YoussefAswad@users.noreply.github.com>
2023-09-15 19:14:06 +0300

    Added new config and schema

---
+++ validate_scheme.py
@@ -0,0 +1,22 @@
+import json
+import yaml
+from jsonschema import exceptions, validate
+
+# Load the schema
+with open("schema.jsonc", "r") as f:
+    schema = json.load(f)
+
+# Load the data
+with open("./config/config.yaml", "r") as f:
+    data = yaml.safe_load(f)
+
+# Validate the data
+try:
+    validate(data, schema)
+    print("Validated successfully")
+except exceptions.ValidationError as e:
+    print(e)
+    exit(1)
+except exceptions.SchemaError as e:
+    print(e)
+    exit(1)
